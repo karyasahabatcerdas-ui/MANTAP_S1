e8cf996dc11df3ac29ce3149500b8ff330b893db
nofadin <karyasahabatcerdas@gmail.com>
2026-03-28 15:37:45 +0700

    cari res dulu dari server

--- script.js
+++ script.js
@@ -3790,7 +3790,8 @@ if (temp_Asset_Files && temp_Asset_Files.length > 0) {
       assetPayload,
       userData: dataArray // Kita kirim dataArray sebagai userData tambahan untuk di-inject ke sheet
     });
-    console.log("pesan dari server :", res.msg)
+    console.log("pesan dari server :", res.msg);
+    console.log("pesan dari server :", res.status);
     console.log("assetpayload :",assetPayload);
     console.table(assetPayload);
 
